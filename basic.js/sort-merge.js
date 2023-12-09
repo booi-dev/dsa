a981940756b4eb176cafb94d140d4f21cf80248e
booi-dev <booimangang@gmail.com>
2023-12-09 12:06:24 +0530

    add: create merge sort algorithm

---
+++ basic.js/sort-merge.js
@@ -0,0 +1,26 @@
+// sort the arr in ascending order
+
+const arr = [-6, 20, 8, -2, 4];
+
+const merge = (leftArr, rightArr) => {
+  const sorted = [];
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] < rightArr[0]) {
+      sorted.push(leftArr.shift());
+    } else {
+      sorted.push(rightArr.shift());
+    }
+  }
+  return [...sorted, ...leftArr, ...rightArr];
+};
+
+const mergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  const leftArr = arr.slice(0, mid);
+  const rightArr = arr.slice(mid);
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
+};
+
+console.log(mergeSort(arr)); // [-6, -2, 4, 8, 20]
