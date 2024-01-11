68568ff6e5506eb75ce98ee2066828baaf106cd9
booi-dev <booimangang@gmail.com>
2024-01-11 18:28:56 +0530

    add: add new node n delete node

--- ds.js/linkedlist.js
+++ ds.js/linkedlist.js
@@ -1,6 +1,6 @@
 class Node {
   constructor(value) {
-    this.value = null;
+    this.value = value;
     this.next = null;
   }
 }
@@ -9,6 +9,7 @@ class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
+    this.size = 0;
   }
 
   addNode(value) {
@@ -20,6 +21,74 @@ class LinkedList {
       this.tail.next = newNode;
       this.tail = newNode;
     }
+
+    this.size += 1;
+  }
+
+  addNodeAtNth(value, position) {
+    if (position > this.size || position < 1) {
+      console.log("position invalid to add");
+      return;
+    }
+
+    let newNode = new Node(value);
+    let current = this.head;
+
+    let _position = 1;
+
+    while (current) {
+      _position += 1;
+
+      if (_position === position) {
+        let before = current;
+        let after = current.next;
+
+        before.next = newNode;
+        newNode.next = after;
+
+        this.size += 1;
+
+        return;
+      }
+
+      current = current.next;
+    }
+  }
+
+  deleteNodeAtNth(position) {
+    if (position > this.size || position < 1) {
+      console.log("position invalid to delete");
+      return;
+    }
+
+    let prev = null;
+    let current = this.head;
+    let _position = 1;
+
+    while (current) {
+      if (_position === position) {
+        if (position == 1) {
+          this.head = current.next;
+          this.size -= 1;
+          return;
+        }
+
+        prev.next = current.next;
+
+        this.size -= 1;
+
+        return;
+      }
+
+      _position += 1;
+
+      prev = current ?? prev.next;
+      current = current.next;
+    }
+  }
+
+  getSize() {
+    console.log("Size:", this.size);
   }
 
   printList() {
@@ -35,4 +104,9 @@ const myList = new LinkedList();
 myList.addNode(1);
 myList.addNode(2);
 myList.addNode(3);
+myList.addNode(4);
+myList.addNode(5);
+myList.addNodeAtNth(69, 2);
+myList.deleteNodeAtNth(1);
 myList.printList();
+myList.getSize();
